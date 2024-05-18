e1c1919e0e0e23a47e0d31df1eea9cf62189fe03
이재완 <162649228+leej1045@users.noreply.github.com>
2024-05-18 23:30:28 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+a=int(input())
+if a >= 90:
+    print("A")
+elif 89 >= a >= 80:
+    print("B")
+elif 79 >= a >= 70:
+    print("C")
+elif 69 >= a >= 60:
+    print("D")
+elif a == 100:
+    print("A")
+elif a == 0:
+    print("F")
+else:
+    print("F")
