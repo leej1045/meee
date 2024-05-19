5eb4a0c24594ed308f3a2106edd0e09a559f05f5
이재완 <162649228+leej1045@users.noreply.github.com>
2024-05-19 21:11:59 +0900

    Create ee.py

---
+++ ee.py
@@ -0,0 +1,9 @@
+a=input('Element? ')
+b=input('Element? ')
+c=input('Element? ')
+d=input('Element? ')
+e=input('Element? ')
+f=input('Element? ')
+g=int(input('Index? '))
+h=[a,b,c,d,e,f]
+print(h[g:7])
